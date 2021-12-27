9eb61ccbd8f58cfcec3b03ea4753afbecff9ad71
Urakova Anna <gram_anna@mail.ru>
2021-12-27 11:21:33 +0400

    feature: верстка основного блока

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './main.scss';
 
+const form = document.querySelector('.send-info');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+
+  if (formData.get('interests').length == 0) {
+    form.querySelector('.send-info__input').classList.add('send-info__input--error');
+  }
+
+  console.log('Вы ввели:', formData.get('interests'));
+  form.reset()
+})
